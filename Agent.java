cf1daeb8b70928d2f4ba6d8f9e209a5b2e431f32
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-02 01:39:38 +0800

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -1,4 +1,45 @@
 public abstract class Agent
 {
+    protected boolean status;
+    protected int gems;
+    protected int gemsInTent;
+    protected ArrayList<Artifact> ownedArtifact;
+
+    public int getGems()
+    {
+        retrun this.gems;
+    }
     
+    public void setGems(int gems)
+    {
+        this.gems = gems;
+    }
+    
+    public int getGemsInTent()
+    {
+        return this.gemsInTent;
+    }
+    
+    public int setGemsInTent(int gemsInTent)
+    {
+        this.gemsInTent = gemsInTent;
+    }
+    
+    public void addGems(int gems)
+    {
+        this.gems += gems;
+    }
+
+    public void putGemsIntoTent()
+    {
+        this.gemsInTent += this.gems;
+        this.gems = 0;
+    }
+
+    public void lostAllGems()
+    {
+        this.gems = 0;
+    }
+
+    public abstract void act();
 }
