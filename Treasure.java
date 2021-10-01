8a2359dd1ec5cbb60531e9a50a6a8f44baff3836
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-02 00:05:14 +0800

    Update Treasure.java

--- Treasure.java
+++ Treasure.java
@@ -1,6 +1,11 @@
 public abstract class Treasure extends Card
 {
     protected int value;
+    
+    public Treasure(int number)
+    {
+        super(number);
+    }
 
     public int getValue()
     {
