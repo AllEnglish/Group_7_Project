e95d173b9544d3e448112d57baaf2843321f3a77
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-02 00:08:34 +0800

    Create Gemstone.java

---
+++ Gemstone.java
@@ -0,0 +1,19 @@
+public class Gemstone extends Treasure
+{
+    protected final int originalValue;
+
+    public int getOriginalValue()
+    {
+        return this.originalValue;
+    }
+
+    public String name()
+    {
+        return “Gemstone”; // ???
+    }
+
+    public int share(int member)
+    {
+        // do something...
+    }
+}
