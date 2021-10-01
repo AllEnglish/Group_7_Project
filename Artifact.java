07ed973e5831d227ea5518c86964d05c7480df66
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-02 00:58:08 +0800

    Update Artifact.java

--- Artifact.java
+++ Artifact.java
@@ -10,4 +10,11 @@ public class Artifact extends Treasure
     {
         return "name";
     }
+    
+    @Override
+    public void share(ArrayList<Agent> receivers)
+    {
+        if (receivers.size() == 1 && !receivers.get(0).status)
+            receivers.get(0).ownedArtifact.add(this);
+    }
 }
