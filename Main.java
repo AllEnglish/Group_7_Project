865e7705855b25d3cac94ebf255e7bd54c930a10
minhsia0809 <61908693+minhsia0809@users.noreply.github.com>
2021-10-01 23:47:36 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("weed haha 2");
-        System.out.println("teeeeeeeeeeest")
+        System.out.println("teeeeeeeeeeest");
     }
 }
