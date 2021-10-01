72aa4bcf9459684d808258942a548680b5d66c9b
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-01 23:53:16 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,16 +1,16 @@
 public abstract class Card
 {
-	protected int number;
+    protected int number;
 
-	public Card(int number)
-	{
-		this.number = number;
+    public Card(int number)
+    {
+        this.number = number;
     }
 
-	public abstract String name();
+    public abstract String name();
 
-	public int getNumber()
-	{
-		return this.number;
+    public int getNumber()
+    {
+        return this.number;
     }
 }
