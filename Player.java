08ea1247744f9194d6e22f59454973199ece5379
minhsia0809 <61908693+minhsia0809@users.noreply.github.com>
2021-10-05 15:03:24 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,25 @@
+import java.util.*;
+
 public class Player extends Agent
 {    
-    public Player()
+    public Player(int type)
     {
+        super(type);
+    }
+    
+    // 
+    public void act()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        String test = keyboard.nextLine();
         
+        if(test.equals("y"))
+        {
+            setInExploring(true);
+        }
+        else
+        {
+            setInExploring(false);
+        }
     }
 }
