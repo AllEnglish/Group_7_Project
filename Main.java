175d590bbbb8ab873d993ff0d1949b5ffdc43419
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-05 16:55:05 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("weed haha 2");
-        System.out.println("teeeeeeeeeeest");
+        Thread t = new Thread(new Game(5));
+        t.start();
     }
 }
