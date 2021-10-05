a3ba653ac065ca6e4c8b3757b3c98f149fac6e29
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-05 16:54:27 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -17,7 +17,8 @@ public class Game implements Runnable
     @Override
     public void run()
     {
-        
+        this.initializeDeck();
+        System.out.println("HI");
     }
     
     private void initializeDeck()
@@ -37,6 +38,22 @@ public class Game implements Runnable
         this.deck.add(new Hazard(4));
         this.deck.add(new Hazard(4));
         this.deck.add(new Hazard(4));
+        
+        this.deck.add(new Gemstone(0, 1));
+        this.deck.add(new Gemstone(1, 2));
+        this.deck.add(new Gemstone(2, 3));
+        this.deck.add(new Gemstone(3, 4));
+        this.deck.add(new Gemstone(4, 5));
+        this.deck.add(new Gemstone(4, 5));
+        this.deck.add(new Gemstone(5, 7));
+        this.deck.add(new Gemstone(5, 7));
+        this.deck.add(new Gemstone(6, 9));
+        this.deck.add(new Gemstone(7, 11));
+        this.deck.add(new Gemstone(7, 11));
+        this.deck.add(new Gemstone(8, 13));
+        this.deck.add(new Gemstone(9, 14));
+        this.deck.add(new Gemstone(10, 15));
+        this.deck.add(new Gemstone(11, 17));
     }
 
     public void deckShffule()
