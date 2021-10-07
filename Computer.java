c1a6a631e04a0dc85d7cfe6131f910e123f3a2e5
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-08 04:31:12 +0800

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -9,7 +9,7 @@ public class Computer extends Agent
     
     public void act()
     {
-        int millisecond = (int)(Math.random() * 2000);
+        int millisecond = (int)(Math.random() * 1200);
         try
         {
             Thread.sleep(millisecond);
