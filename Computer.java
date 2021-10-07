633086003068f98c29c8f38e2169a1dac2461f09
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-08 03:36:05 +0800

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -18,7 +18,7 @@ public class Computer extends Agent
         {
         }
         
-        this.setInExploring(Math.random() < 0.75);
+        this.setInExploring(Math.random() < 0.6);
         System.out.println("explorer " + this.type + " just makes it's decision! (" + (millisecond / 1000.0) + ")");
     }
 }
