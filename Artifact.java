a1d060f66dea9da89bf277fe9cc1ae7c198f3fd2
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-09 00:09:48 +0800

    Update Artifact.java

--- Artifact.java
+++ Artifact.java
@@ -44,13 +44,16 @@ public class Artifact extends Treasure
     public void share(ArrayList<Agent> receivers)
     {
         if (receivers.size() == 1 && !receivers.get(0).isInExploring())
+        {
             receivers.get(0).possessionOfArtifacts.add(this);
+            this.takenAway = true;
+        }
     }
     
     @Override
     public String toString()
     {
-        return String.format("<\u001B[33m%s\u001B[0m>", this.name());
+        return String.format("<\u001B[33m%s\u001B[0m>", (this.takenAway ? "---" : this.name()));
         // return String.format("%s <Artifact %d with value %d>", this.name(), this.number, this.value);
     }
 }
