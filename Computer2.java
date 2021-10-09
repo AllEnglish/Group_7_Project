b9b5bd30a8734e7dd7a4928f1d1020c7529041f1
minhsia0809 <61908693+minhsia0809@users.noreply.github.com>
2021-10-09 23:09:28 +0800

    Create Computer2.java

---
+++ Computer2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Computer2 extends Agent
+{    
+    public Computer(int type)
+    {
+        super(type);
+    }
+    
+    public void act(List<Card> path)
+    {
+        int millisecond = (int)(Math.random() * 400);
+        try
+        {
+            Thread.sleep(millisecond);
+        }
+        catch (InterruptedException e) {}
+        
+        /* To-do */
+        
+        this.setInExploring(Math.random() < 0.85);
+        
+        /* End of to-do */
+        
+        System.out.println("    " + this.getClass().getName() + " " + this.type + " decided! (" + (millisecond / 1000.0) + "s)");
+    }
+}
