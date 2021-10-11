0ae1e6d8511037af7b530e12c355d37a9ab5ff03
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-11 13:38:28 +0800

    Update CompDefault.java

--- CompDefault.java
+++ CompDefault.java
@@ -8,7 +8,7 @@ public class CompDefault extends Agent
     }
     
     @Override
-    public boolean decision(int countOfExplorersInTomb, List<Card> path, List<Hazard> removedHazard);
+    public boolean decision(int countOfExplorersInTomb, List<Card> path, List<Hazard> removedHazard)
     {
         try
         {
