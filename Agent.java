7ce74f11a15e5963a07baa8da14cf42ff510cd7d
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-12 15:46:04 +0800

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -80,7 +80,7 @@ public abstract class Agent // implements Comparable<Agent>
         // NOTICE 1: for some reason, this method should be final...
         // NOTICE 2: we cannot pass "game.path" into decision() directly, but the deep-copied one.
         
-        for (Card c : game.path)
+        for (Card c : game.getPath())
         {
             try
             {
