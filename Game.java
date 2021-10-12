cb74596b346a88c2502cb566d2febe7e9e4d40db
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-12 15:08:14 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -24,8 +24,8 @@ public class Game
         this.lastOccurredHazard = null;
         
         // just for testing
-        this.explorers.add(new CompSammi(0));
-        this.explorers.add(new CompDefault(1));
+        this.explorers.add(new Player(0));
+        this.explorers.add(new CompSammi(1));
         this.explorers.add(new CompDefault(2));
         this.explorers.add(new CompDefault(3));
         this.explorers.add(new CompSammi(4));
