98e7d75ce8f216a02f91ba33efa2b9508e008c19
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-13 23:10:54 +0800

    Update CompSammi.java

--- CompSammi.java
+++ CompSammi.java
@@ -10,26 +10,11 @@ public class CompSammi extends Agent
     @Override
     public boolean decision(int countOfExplorersInTomb, List<Card> path, List<Hazard> removedHazard)
     {
-        try
-        {
-            int millisecond = (int)(Math.random() * 500);
-            Thread.sleep(millisecond);
-            System.out.println("    " + this.getClass().getName() + " " + this.type + " decided! (" + (millisecond / 1000.0) + "s)");
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-        
-        /* To-do */
-        
         if (path.size() <= 3)
             return true;
         else if (path.get(path.size() - 1) instanceof Treasure)
             return (Math.random() < 0.85);
         else
             return (Math.random() < 0.55);
-        
-        /* End of to-do */
     }
 }
