37d39858fcfa968ae7cc89e8566ae4452266e3c9
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-10-13 23:11:50 +0800

    Update CompDefault.java

--- CompDefault.java
+++ CompDefault.java
@@ -10,21 +10,6 @@ public class CompDefault extends Agent
     @Override
     public boolean decision(int countOfExplorersInTomb, List<Card> path, List<Hazard> removedHazard)
     {
-        try
-        {
-            int millisecond = (int)(Math.random() * 200);
-            Thread.sleep(millisecond);
-            System.out.println("    " + this.getClass().getName() + " " + this.type + " decided! (" + (millisecond / 1000.0) + "s)");
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-        
-        /* To-do */
-        
         return (Math.random() < 0.85);
-        
-        /* End of to-do */
     }
 }
