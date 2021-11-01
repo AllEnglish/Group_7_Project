b488f1bafdc6ddcfc1812a5f491667ca74500579
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-11-01 17:27:40 +0800

    Create GameData.java

---
+++ GameData.java
@@ -0,0 +1,14 @@
+public class GameData
+{
+    private int playerNum;
+    
+    public GameData(Game g)
+    {
+        
+    }
+    
+    public int getPlayerNum()
+    {
+        return this.playerNum;
+    }
+}
