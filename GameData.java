d0517f50523ea89f8157dba9d66ae3e2d8c48800
AllEnglish <91744278+AllEnglish@users.noreply.github.com>
2021-11-01 17:36:31 +0800

    Update GameData.java

--- GameData.java
+++ GameData.java
@@ -1,14 +1,33 @@
 public class GameData
 {
     private int playerNum;
+    private List<Card> path;
     
     public GameData(Game g)
     {
+        this.playerNum = 0;
+        this.path = new ArrayList<>();
         
+        for (Card c : g.getPath())
+        {
+            try
+            {
+                this.path.add(c.clone());
+            }
+            catch (CloneNotSupportedException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
     
     public int getPlayerNum()
     {
         return this.playerNum;
+    }
+    
+    public List<Card> getPath()
+    {
+        return this.path;
     }
 }
